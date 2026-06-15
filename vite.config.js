e817fa6445d302cb09c89fbac225f675ba1d02df
Jose Pretelin <josepretelin@MacBookPro-5493.lan>
2026-06-15 15:01:56 -0400

    GymTrack PWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,35 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+
+    VitePWA({
+      registerType: "autoUpdate",
+
+      manifest: {
+        name: "GymTrack",
+        short_name: "GymTrack",
+        description: "Seguimiento de entrenamientos y progreso físico",
+        theme_color: "#0f172a",
+        background_color: "#0f172a",
+        display: "standalone",
+
+        icons: [
+          {
+            src: "/icon-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
